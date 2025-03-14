37846fe376db1325e06169ab7c34946c482c24c0
Sanya Singh <sanya_singh@epam.com>
2025-03-14 14:39:00 +0530

    solved task03

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,15 @@
+//exports.handler = async (event) => {
+//    // TODO implement
+//    const response = {
+//        statusCode: 200,
+//        body: JSON.stringify('Hello from Lambda!'),
+//    };
+//    return response;
+//};
+exports.handler = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Hello from Lambda' }),
+    };
+};
+
