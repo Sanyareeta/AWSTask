889cb16957c33390d21bc900f0a65544959c84af
Sanya Singh <sanya_singh@epam.com>
2025-03-20 12:45:31 +0530

    solved task06  Conflicts:
    f

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -5,7 +5,6 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-
 import java.util.HashMap;
 import java.util.Map;
 
